3fd66829be62d7cf9bc9a7ffbb93c4c4b4ae890e
col3name <mmikusho@instaloper.com>
2024-08-13 13:14:18 +0300

    feat: disable scaling

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
         <head>
             <meta charSet="utf-8"/>
             <title>Title</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
         </head>
         <body className={inter.className}>
         <QueryProviders>
